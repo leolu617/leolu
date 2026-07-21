133f235e535d9753294ef972a1c49af37f30e3f1
Leo Lu <t1993617@gmail.com>
2026-07-21 15:58:08 +0800

    Fix vite build permission

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,28 @@
 import React from "react";
 
 import {
-BrowserRouter,
-Routes,
-Route
+  HashRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 }
-from "react-router-dom";
-
-
-import MainLayout 
-from "./layouts/MainLayout";
-
-
-import Home 
-from "./pages/Home";
-
-
-import About 
-from "./pages/About";
-
-
-import Projects 
-from "./pages/Projects";
-
-
-
-function App(){
-
-
-return (
-
-<BrowserRouter>
-
-
-<Routes>
-
-
-<Route element={<MainLayout />}>
-
-
-<Route 
-path="/"
-element={<Home />}
-/>
-
-
-<Route
-path="/about"
-element={<About />}
-/>
-
-
-<Route
-path="/projects"
-element={<Projects />}
-/>
-
-
-</Route>
-
-
-</Routes>
-
-
-</BrowserRouter>
-
-)
-
-}
-
 
 export default App;
